ea1cb69909e8065a26d7202163daf51ceb99b456
dy3722 <dy3722@bs.amalnet.k12.il>
2025-01-07 04:10:19 +0200

    Matala 1

--- app/src/main/java/com/example/ex06031/MainActivity.java
+++ app/src/main/java/com/example/ex06031/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex06031;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView textV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        textV = findViewById(R.id.textV);
+    }
+
+    public void ohyea(View view) {
+        textV.setTextSize(30);
+        textV.setText("Oh, yea, I’ve been clicked!");
     }
 }
